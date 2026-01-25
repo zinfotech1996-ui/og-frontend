c7a0ef2ef227112d542a7f9dcf94565446002aa3
GeLoader <zinfotech1996@gmail.com>
2026-01-25 22:54:21 +0800

    1

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                 <Routes>
                   <Route path="/login" element={<LoginPage />} />
                   <Route
-                    path="/"
+                    path="./"
                     element={
                       <ProtectedRoute>
                         <DashboardLayout />
